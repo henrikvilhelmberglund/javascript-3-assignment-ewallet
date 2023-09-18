35c8a8cfdf78b566f3347b9680d4507401cff6b9
Henrik Berglund <110549389+henrikvilhelmberglund@users.noreply.github.com>
2023-09-18 10:40:55 +0200

    feat: :sparkles: add routes

---
+++ src/pages/cards/index.jsx
@@ -0,0 +1,7 @@
+export default function Index() {
+  return (
+    <>
+      <h1 className="text-5xl">Cards</h1>
+    </>
+  );
+}
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,13 +1,23 @@
 import { useDispatch, useSelector } from "react-redux";
 import { increment } from "../redux/ewalletSlice";
+import { Link } from "react-router-dom";
 
 export default function Index() {
   const dispatch = useDispatch();
   const { count } = useSelector((store) => store.ewallet);
   return (
     <>
-      <p className="text-4xl">hi I am index</p>
-      <button onClick={() => dispatch(increment())}>{count}</button>
+      <h1 className="text-4xl">E-Wallet</h1>
+      <h2 className="text-2xl">
+        Welcome to your E-Wallet! Click the link below to view your cards.
+      </h2>
+      <div className="mt-12">
+        <Link
+          className="text-xl hover:bg-blue-100 transition-all bg-blue-200 p-4 rounded-lg text-blue-600"
+          to="/cards">
+          View your cards
+        </Link>
+      </div>
     </>
   );
 }

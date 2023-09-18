baa8f3144005ce4b8d7b6b55dceb12565c9e6634
Henrik Berglund <110549389+henrikvilhelmberglund@users.noreply.github.com>
2023-09-18 11:57:44 +0200

    feat: :package: add UnoCSS icon preset and icons

--- uno.config.js
+++ uno.config.js
@@ -1,4 +1,5 @@
 import { defineConfig, presetUno } from "unocss";
+import presetIcons from "@unocss/preset-icons";
 
 export default defineConfig({
   // ...UnoCSS options
@@ -6,9 +7,17 @@ export default defineConfig({
     presetUno({
       dark: "class",
     }),
+    presetIcons(),
   ],
-  shortcuts: {
-    "nav-button":
-      "rounded-lg p-4 outline-1 outline-solid outline-black hover:bg-blue-100 transition-all text-2xl",
-  },
+  shortcuts: [
+    {
+      "nav-button":
+        "rounded-lg p-4 outline-1 outline-solid outline-black hover:bg-blue-100 transition-all text-2xl",
+    },
+    [
+      /^btn-(.*)$/,
+      ([, c]) =>
+        `bg-${c}-400 text-${c}-800 text-xl p-4 rounded-lg transition-all hover:bg-${c}-300`,
+    ],
+  ],
 });

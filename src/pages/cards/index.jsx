f838217a9e7152e2cfced93c5ec253ebce5275de
Henrik Berglund <110549389+henrikvilhelmberglund@users.noreply.github.com>
2023-09-18 11:59:03 +0200

    feat: :sparkles: update cards route

--- src/pages/cards/index.jsx
+++ src/pages/cards/index.jsx
@@ -1,7 +1,22 @@
+import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+import Card from "../_components/Card";
 export default function Index() {
+  const { cards } = useSelector((store) => store.ewallet);
   return (
     <>
       <h1 className="text-5xl">Cards</h1>
+      <section>
+        <h2>Here you can view your cards.</h2>
+        {cards?.map((card) => (
+          <Card {...card}></Card>
+        ))}
+      </section>
+      <div className="mt-12">
+        <Link to="/addcard" className="btn-green">
+          Add new card
+        </Link>
+      </div>
     </>
   );
 }

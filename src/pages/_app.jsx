c3cfab8152897ac3ac09170ae08b2af406209866
Henrik Berglund <110549389+henrikvilhelmberglund@users.noreply.github.com>
2023-09-18 14:33:08 +0200

    feat: :lipstick: styling

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -3,7 +3,7 @@ import { Link, Outlet } from "react-router-dom";
 export default function Layout() {
   return (
     <>
-      <div className="w-screen h-screen bg-gradient-to-b from-white to-sky-200 dark:bg-sky-900">
+      <div className="w-screen min-h-screen  bg-gradient-to-b from-white to-sky-200 dark:bg-sky-900">
         <header>
           <nav className="flex flex justify-center gap-12 items-center bg-slate-100 py-2">
             <Link className="nav-button" to="/">
@@ -14,10 +14,10 @@ export default function Layout() {
             </Link>
           </nav>
         </header>
-        <main className="flex flex-col items-center md:mt-32">
+        <main className="flex flex-col items-center md:mt-10">
           <Outlet />
         </main>
-        <footer className="absolute bottom-2 right-2">
+        <footer className="fixed bottom-2 right-2">
           Copyright ©2023 Ankwallet
         </footer>
       </div>

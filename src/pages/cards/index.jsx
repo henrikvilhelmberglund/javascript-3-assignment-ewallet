a86f79c7a850e22b336a880ff6057ee7ef0a1c0f
Henrik Berglund <110549389+henrikvilhelmberglund@users.noreply.github.com>
2023-09-18 14:34:15 +0200

    feat: :sparkles: loop out active card and inactive cards

--- src/pages/cards/index.jsx
+++ src/pages/cards/index.jsx
@@ -6,11 +6,26 @@ export default function Index() {
   return (
     <>
       <h1 className="text-5xl">Cards</h1>
-      <section>
+      <section className="flex flex-col gap-6 items-center">
         <h2>Here you can view your cards.</h2>
-        {cards?.map((card) => (
-          <Card {...card}></Card>
-        ))}
+        <p>Active card:</p>
+
+        {cards
+          ?.filter((card) => card.active === true)
+          .map((card, i) => (
+            <Card {...card} i={i} key={i}></Card>
+          ))}
+
+        <p>Inactive cards:</p>
+        <div className="w-full flex items-center">
+          <div className="relative h-[400px] top-0 left-0">
+            {cards
+              ?.filter((card) => card.active === false)
+              .map((card, i) => (
+                <Card {...card} i={i} key={i}></Card>
+              ))}
+          </div>
+        </div>
       </section>
       <div className="mt-12">
         <Link to="/addcard" className="btn-green">

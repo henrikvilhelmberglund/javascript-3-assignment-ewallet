2e2bb5af5f09da0561f249a97cd99c0bb24a8add
Henrik Berglund <110549389+henrikvilhelmberglund@users.noreply.github.com>
2023-09-18 10:40:38 +0200

    feat: :lipstick: add layout styling

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -3,8 +3,9 @@ import { Outlet } from "react-router-dom";
 export default function Layout() {
   return (
     <>
-      <p>hi I am layout</p>
-      <Outlet />
+      <main className="w-screen h-screen flex flex-col items-center md:mt-32 bg-gradient-to-b from-white to-sky-200 dark:bg-sky-900">
+        <Outlet />
+      </main>
     </>
   );
 }

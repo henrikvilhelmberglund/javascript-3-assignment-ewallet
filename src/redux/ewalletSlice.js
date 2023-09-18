7902f42c1e6518d3364b51af7cee2d997f88be74
Henrik Berglund <110549389+henrikvilhelmberglund@users.noreply.github.com>
2023-09-18 11:58:45 +0200

    feat: :sparkles: add initialstate for one card

--- src/redux/ewalletSlice.js
+++ src/redux/ewalletSlice.js
@@ -1,16 +1,30 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
-export const getPosts = createAsyncThunk("postsSlice/getPosts", async () => {
-  const response = await fetch("https://jsonplaceholder.typicode.com/posts");
-  const data = await response.json();
-  return data;
-});
+export const getPosts = createAsyncThunk(
+  "ewalletSlice/getStartUser",
+  async () => {
+    const response = await fetch("https://jsonplaceholder.typicode.com/posts");
+    const data = await response.json();
+    return data;
+  }
+);
 const ewalletSlice = createSlice({
   name: "ewalletSlice",
-  initialState: { posts: [], status: null, count: 0 },
+  initialState: {
+    cards: [
+      {
+        name: "Christoffer Wallenberg",
+        number: 1234567891011123,
+        validThru: "12/22",
+        vendor: "BitcoinCard",
+        ccv: 376,
+      },
+    ],
+    activeCard: null,
+  },
   reducers: {
-    increment: (state, action) => {
+    setActiveCard: (state, action) => {
       console.log(action);
-      state.count += 1;
+      state.activeCard += action.payload;
     },
   },
   extraReducers: (builder) => {

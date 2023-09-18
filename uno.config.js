13ee8fe133adcffb0c8d4791b62e1eb63a113e64
Henrik Berglund <110549389+henrikvilhelmberglund@users.noreply.github.com>
2023-09-18 10:57:17 +0200

    feat: :sparkles: add unocss shortcut for nav-button

--- uno.config.js
+++ uno.config.js
@@ -7,4 +7,8 @@ export default defineConfig({
       dark: "class",
     }),
   ],
+  shortcuts: {
+    "nav-button":
+      "rounded-lg p-4 outline-1 outline-solid outline-black hover:bg-blue-100 transition-all text-2xl",
+  },
 });

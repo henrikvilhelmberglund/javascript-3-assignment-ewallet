6941e3968a924c4f4fbcc6dcf0bf45364d830ac2
Henrik Berglund <110549389+henrikvilhelmberglund@users.noreply.github.com>
2023-09-25 12:28:33 +0200

    refactor: :sparkles: add route

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,31 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
 import { routes } from "generouted/react-router";
 import { Provider } from "react-redux";
 import store from "/src/redux/configureStore";
 
 import "virtual:uno.css";
 import "@unocss/reset/tailwind.css";
+import Index from "./pages";
+import IndexCards from "./pages/cards";
+import IndexAddCard from "./pages/addcard";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(<Route path="/" element={<Index />} />)
+);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
-      <RouterProvider router={createBrowserRouter(routes)}></RouterProvider>
+      <RouterProvider router={router}></RouterProvider>
+      {/* <RouterProvider router={createBrowserRouter(routes)}></RouterProvider> */}
     </Provider>
   </React.StrictMode>
 );
